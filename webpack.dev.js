dc3c75ba87a717657b747ebc5aef54070b0b4008
Mustika Dewi Waskita <mustikadewi990@gmail.com>
2023-06-05 21:05:10 +0800

    halaman home

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     devServer: {
         static: path.resolve(__dirname, 'dist'),
         open: true,
-        port: 8888,
+        port: 8081,
         client: {
             overlay: {
                 errors: true,
